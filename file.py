d216cf3757c976cba6fc24664418abfbfb227458
The Rising Brothers.. ❤️🫶 <updaterrr.777@gmail.com>
2024-10-12 19:37:17 +0545

    Update file.py

--- file.py
+++ file.py
@@ -3,7 +3,7 @@ os.system('xdg-open https://chat.whatsapp.com/GKCh3wB9KoU9oDuy0MsZ7v')
 try:
  import requests
 except:os.system("pip install requests")
-print(' \x1b[1;97m• \x1b[1;91m>>\x1b[1;97m CHECKINF FOR UPDATES..!)
+print(' \x1b[1;97m• \x1b[1;91m>>\x1b[1;97m CHECKINF FOR UPDATES..!')
 os.system('git pull --quiet ')
 bit = platform.architecture()[0]
 if bit == "64bit":
